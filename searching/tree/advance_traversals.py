702fd0cde136ba61a11998be92f3613f45dad699
EashanKaushik <EashanK16@gmail.com>
2021-05-26 17:43:14 +0530

    26-05-2021

--- searching/tree/advance_traversals.py
+++ searching/tree/advance_traversals.py
@@ -31,8 +31,98 @@ def diagonal_traversal(root, value):
             if node == curr_node.data:
                 return True
 
+def boundary_traversal(root, value):
+
+    # left side
+    curr_node = root
+
+    while 1:
+        # print(curr_node.data)
+        if curr_node.data == value:
+            return True
+
+        if curr_node.left:
+            curr_node = curr_node.left
+        elif curr_node.right:
+            curr_node = curr_node.right
+        else:
+            break
+
+    # leaf nodes
+    queue = list()
+    queue.append(root)
+
+    while len(queue) > 0:
+        curr_node = queue.pop(0)
+
+        if not curr_node.left and not curr_node.right:
+            # print(curr_node.data)
+            if curr_node.data == value:
+                return True
+        else:
+            if curr_node.left:
+                queue.append(curr_node.left)
+            if curr_node.right:
+                queue.append(curr_node.right)
+
+    # right nodes
+    curr_node = root.right
+    # stack = list()
+    while 1:
+        # stack.append(curr_node.data)
+        if curr_node.data == value:
+            return True
+
+        if curr_node.right:
+            curr_node = curr_node.right
+        elif curr_node.left:
+            curr_node = curr_node.left
+        else:
+            break
+
+    # while len(stack) > 0:
+    #     print(stack.pop())
+
+def spiral_traversal(root, value):
+
+    l_r_stack = list()
+    r_l_stack = list()
+
+    l_r_stack.append(root)
+
+    while len(l_r_stack) > 0 or len(r_l_stack) > 0:
+
+        while len(l_r_stack) > 0:
+            curr_node = l_r_stack.pop()
+
+            # print(curr_node.data)
+
+            if curr_node.data == value:
+                return True
+
+            if curr_node.right:
+                r_l_stack.append(curr_node.right)
+
+            if curr_node.left:
+                r_l_stack.append(curr_node.left)
+
+        while len(r_l_stack) > 0:
+
+            curr_node = r_l_stack.pop()
+
+            # print(curr_node.data)
+
+            if curr_node.data == value:
+                return True
+
+            if curr_node.left:
+                l_r_stack.append(curr_node.left)
+
+            if curr_node.right:
+                l_r_stack.append(curr_node.right)
+
 if __name__ == '__main__':
 
     root = make_tree()
 
-    print(diagonal_traversal(root, 4))
+    print(spiral_traversal(root, 4))
