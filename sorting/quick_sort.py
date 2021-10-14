e5bd391e029cb47e650d5665647ff57590b9b343
EashanKaushik <Eashank16@gmail.com>
2021-10-14 15:00:43 -0400

    quick sort

--- sorting/quick_sort.py
+++ sorting/quick_sort.py
@@ -4,42 +4,30 @@
 
 
 # this implementaion has worst space complexity of O(n^2)
-# def quick_sort(arr):
-#     if len(arr) > 1:
-#         pivot = arr.pop()
-#         items_less = [item for item in arr if item < pivot]
-#         items_greater = [item for item in arr if item >= pivot]
-#
-#         return quick_sort(items_less) + [pivot] + quick_sort(items_greater)
-#     else:
-#         return arr
+def quick_sort(arr, low, high):
 
-# better implementaion
-def quick_sort(arr, left, right):
+    if low < high:
 
-    if left < right:
+        pivot = partition(arr, low, high)
+        
+        quick_sort(arr, low, pivot - 1)
+        quick_sort(arr, pivot + 1, high)
 
-        start = left
-        end = right - 1
-        pivot = right
+def partition(arr, low, high):
 
-        while start < pivot:
+    pivot = arr[high]
+    lower_pivot = low - 1
 
-            if arr[pivot] < arr[start]:
-                if pivot == start + 1:
-                    arr[pivot], arr[start] = arr[start], arr[pivot]
-                    pivot -= 1
-                    continue
-                arr[pivot - 1], arr[pivot] = arr[pivot], arr[pivot - 1]
-                arr[pivot], arr[start] = arr[start], arr[pivot]
-                pivot -= 1
-                continue
+    for compare in range(low, high):
 
-            start += 1
+        if arr[compare] <= arr[pivot]:
+            lower_pivot += 1
+            arr[lower_pivot], arr[compare] = arr[compare], arr[lower_pivot]
+    
+    arr[lower_pivot + 1], arr[high] = arr[high], arr[lower_pivot + 1]
 
-        quick_sort(arr, left, pivot - 1)
+    return lower_pivot + 1
 
-        quick_sort(arr, pivot + 1, right)
 
 if __name__ == '__main__':
 
